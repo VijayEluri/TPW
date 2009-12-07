83107d185371c21ee750517cbee1b35a7aaf524b
Fabio Stapait <stapait@gmail.com>
2009-12-07 15:24:52 -0200

    retirei a criacao da tabela teste do script

--- files/tables.sql
+++ files/tables.sql
@@ -1,10 +1,3 @@
-create table teste (
-	codigo int not null,
-	descricao varchar(100) not null,
-	valor float,
-	primary key (codigo)
-);
-
 create table sites_rss (
 	link varchar(255),
 	site varchar(255),
